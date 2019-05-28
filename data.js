6820ac10f4548156127dcf42ec2ee51a2054785d
Duro Akintunde <duroak@yahoo.com>
2019-05-28 08:12:04 -0500

    added data.js, index.js, main.css, quiz.html, and 2 jpg

---
+++ data.js
@@ -0,0 +1,105 @@
+const QUESTIONS = [
+    {
+        question: 'What premier league team won 13 premier league titles?',
+        answers: [ 
+            'Manchester City',
+            'Arsenal',
+            'Chelsea',
+            'Manchester United'
+        ]
+    },
+    {
+        question: 'Who is the most expensive transfer in the world?',
+        answers: [
+            'Neymar jr.',
+            'Cristiano Ronaldo',
+            'Paul Pogba',
+            'Kylian Mbappe'
+        ]
+    }
+    {
+        question: 'What year did Spain win their first FIFA World Cup?',
+        answers: [
+            '1998',
+            '2010',
+            '2002',
+            '2018'
+        ]
+    }
+    {
+        question: 'Who Infamously Scored a goal with their hands and called it the \"The Hand of God\"?',
+        answers: [
+            'Lionel Messi',
+            'Diego Maradona',
+            'Ronaldo (Brazil)',
+            'Franz Beckenbauer'
+        ]
+    }
+    {
+        question: 'How many ballon d\'ors does Lionel Messi have?',
+        answers: [
+            '4',
+            '6',
+            '3',
+            '5'
+        ]
+    }
+    {
+        question: 'What were the two teams that played in the Champions League Final in 2006?',
+        answers: [
+            'Milan v. Liverpool',
+            'Real Madrid v. Juventus',
+            'Barcelona v. Arsenal',
+            'Porto v. Monaco'
+        ]
+    }
+    {
+        question: 'Which Nation does Eden Harzard play for?',
+        answers: [
+            'France',
+            'Netherlands',
+            'Germany',
+            'Belgium'
+        ]
+    }
+    {
+        question: 'Who is current Manager of Newcastle United?',
+        answers: [
+            'Rafa Benitez',
+            'Jose Mourinho',
+            'Pep Guardiola',
+            'Eddie Howe'
+        ]
+    }
+    {
+        question: 'What is the the nickname of Leicester City F.C.?',
+        answers: [
+            'Reds',
+            'Foxes',
+            'Wolves',
+            'The Swans'
+        ]
+    }
+    {
+        question: 'Who is the top Scorer in Champions League history?',
+        answers: [
+            'Sammuel Eto\'o',
+            'Mo Salah',
+            'Cristiano Ronaldo',
+            'Antoine Griezmann'
+        ]
+    }
+]
+
+const correctAnswer = [
+    'Manchester United',
+    'Neymar Jr',
+    '2010',
+    'Diego Maradona',
+    '5',
+    'Barcelona v. Arsenal',
+    'Belgium',
+    'Rafa Benitez',
+    'Foxes',
+    'Cristiano Ronaldo'
+]
---
+++ index.js
@@ -0,0 +1,24 @@
+let questionNum = 1;
+let answer = 0;
+
+// working on my template literal still
+function generateTemplate (correctAnswer, question) {
+    return `
+    <main role="main">
+    <h1 class="question">${question}</h1>
+
+    <form>
+        <fieldset>
+            <label>
+                <input class="answer></input>
+                <span>${}</span>
+            </label>
+    `
+}
+
+//not done with this function
+function handleStartButton() {
+    $('.startButton').click(function(event){
+
+    });
+}
