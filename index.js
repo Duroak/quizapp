10019a8ddae7f2968b1038cdff19ea5ba2fc3194
Duro Akintunde <duroak@yahoo.com>
2019-06-04 08:20:11 -0500

    multiple files updated

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
-let questionNum = 1;
+let questionNum = 0;
 let answer = 0;
 
-// working on my template literal still
-function generateTemplate (correctAnswer, question) {
-    return `
-    <main role="main">
-    <h1 class="question">${question}</h1>
-
-    <form>
-        <fieldset>
-            <label>
-                <input class="answer></input>
-                <span>${}</span>
-            </label>
-    `
-}
-
-//not done with this function
-function handleStartButton() {
+function renderQuestion(){
+    $("#questionText").text(QUESTIONS[questionNum].question);
+ 
+    $("#answer0").val(QUESTIONS[questionNum].answers[0]);
+    $("#spAns0").text(QUESTIONS[questionNum].answers[0]);
+ 
+    $("#answer1").val(QUESTIONS[questionNum].answers[1]);
+    $("#spAns1").text(QUESTIONS[questionNum].answers[1]);
+ 
+    $("#answer2").val(QUESTIONS[questionNum].answers[2]);
+    $("#spAns2").text(QUESTIONS[questionNum].answers[2]);
+ 
+    $("#answer3").val(QUESTIONS[questionNum].answers[3]);
+    $("#spAns3").text(QUESTIONS[questionNum].answers[3]);
+ 
+ }
+ 
+ function nextQuestion(){
+    questionNum++;
+ }
+ 
+ function handleStartButton() {
     $('.startButton').click(function(event){
-
+        event.preventDefault();
+        $('#startSection').hide();
+        $('#answerBox').hide();
+        $('#quizSection').show();
     });
-}
+ }
+ 
+ function answerQuestion(){
+   $('.#js-submit-button').click(function(event){
+      event.preventDefault();
+      $('#answerBox').show();
+      $()
+   })
+ }
+ 
+ function checkAnswer(){
+ 
+ }
+ 
+ function initializeApp(){
+    handleStartButton();
+ }
+ 
+ $(initializeApp)
